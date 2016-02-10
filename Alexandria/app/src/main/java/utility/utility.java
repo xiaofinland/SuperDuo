9fed260cad8abe00a7659b4aabc94d6094e513a1
Xiao Ma <xiaoma@Xiaos-MBP.home>
2016-02-10 19:57:31 +0200

    add network state permission, add utility to check

---
+++ Alexandria/app/src/main/java/utility/utility.java
@@ -0,0 +1,25 @@
+package utility;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by xiaoma on 10/02/16.
+ */
+public class utility {
+    /**
+          * Returns true if the network is available or about to become available.
+          *
+          * @param c Context used to get the ConnectivityManager
+          * @return
+          */
+        static public boolean isNetworkAvailable(Context c) {
+                ConnectivityManager cm =
+                                (ConnectivityManager)c.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+                        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+               return activeNetwork != null &&
+                       activeNetwork.isConnectedOrConnecting();
+        }
+}
